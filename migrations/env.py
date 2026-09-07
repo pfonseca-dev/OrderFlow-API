58b2065d74932fcb106f3700b1e3bb1ce2ce3a5f
pfonseca-dev <pfmartins2006@gmail.com>
2026-09-07 16:11:10 -0300

    feat: configurar PostgreSQL persistence

--- migrations/env.py
+++ migrations/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import engine_from_config, pool
 
 from app.core.config import settings
 from app.database.session import Base
+from app.models.order import Order  # noqa: F401
 
 # importe seus models aqui para o Alembic enxergá-los.
 # Exemplo:
